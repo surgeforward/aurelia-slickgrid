f267eb011546ab304d514dd8fdedb84692b9438d
ghiscoding <gbeaulac@gmail.com>
2018-01-20 17:43:07 -0500

    update to latest release 1.3.0

--- vendor.84d0370bbca6ababbe29.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{453:function(n,o,p){p(4),n.exports=p(2)}},[453]);
-//# sourceMappingURL=vendor.84d0370bbca6ababbe29.bundle.map
---
+++ vendor.993692787c57b738c3c9.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{454:function(n,o,p){p(4),n.exports=p(2)}},[454]);
+//# sourceMappingURL=vendor.993692787c57b738c3c9.bundle.map
