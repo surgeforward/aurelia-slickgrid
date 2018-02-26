d18ac0b1670550d646b16893f6dd3d316ffc727f
Ghislain B <gbeaulac@gmail.com>
2018-02-26 00:49:40 -0500

    update to latest version 1.6.0

--- vendor.03e37463579baddb9382.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{461:function(n,o,p){p(4),n.exports=p(2)}},[461]);
-//# sourceMappingURL=vendor.03e37463579baddb9382.bundle.map
---
+++ vendor.ae04aeddba261cc242bf.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{467:function(n,o,p){p(5),n.exports=p(2)}},[467]);
+//# sourceMappingURL=vendor.ae04aeddba261cc242bf.bundle.map
