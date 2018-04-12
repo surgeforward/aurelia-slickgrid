da7611293139c9d947009624cc74dcca26bf2a00
Ghislain B <gbeaulac@gmail.com>
2018-04-11 23:00:35 -0400

    update to latest version 1.12.0

---
+++ vendor.6e99e27ef4ee26dc4841.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{516:function(n,o,p){p(5),n.exports=p(1)}},[516]);
+//# sourceMappingURL=vendor.6e99e27ef4ee26dc4841.bundle.map
--- vendor.a9ec8f2386e5df6b25d6.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{514:function(n,o,p){p(5),n.exports=p(1)}},[514]);
-//# sourceMappingURL=vendor.a9ec8f2386e5df6b25d6.bundle.map
