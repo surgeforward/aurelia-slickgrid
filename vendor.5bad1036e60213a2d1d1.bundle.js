82f221530f4f34f9a293b4d57cc75ae72893882a
Ghislain B <gbeaulac@gmail.com>
2018-06-06 22:20:51 -0400

    update to fix missing polyfills on IE

--- vendor.5bad1036e60213a2d1d1.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{512:function(n,o,p){p(5),n.exports=p(1)}},[512]);
-//# sourceMappingURL=vendor.5bad1036e60213a2d1d1.bundle.map
---
+++ vendor.a44f281e9a1d9bdeb030.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{843:function(n,o,p){p(7),n.exports=p(2)}},[843]);
+//# sourceMappingURL=vendor.a44f281e9a1d9bdeb030.bundle.map
